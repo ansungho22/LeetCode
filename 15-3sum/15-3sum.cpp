5fabe6cfe475bd6777923d06be4f501ccfa0a36d
AnSungHo <ansungho22@gmail.com>
2022-03-06 22:20:56 +0900

    Time: 1841 ms (5.01%), Space: 182.8 MB (6.87%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        const size_t len = nums.size();
+        int len = nums.size();
         if (len < 3)
             return {};
-        
-        std::sort(nums.begin(), nums.end());
-        
-        std::set<vector<int>> unique_results;
-        
+        sort(nums.begin(), nums.end());
+        set<vector<int>> unique_results;
         for(int i = 0; i < len - 2; i++) {
-            
             int j = i + 1;
             int k = len - 1;
-            
             while (j < k) {
-            
-                const int sum = nums[i] + nums[j] + nums[k];
+                int sum = nums[i] + nums[j] + nums[k];
                 if (0 == sum) {
                     unique_results.insert({nums[i], nums[j++], nums[k--]});
                 }
@@ -27,9 +20,7 @@ public:
                     k--;
             }
         }
-        
         vector<vector<int>> results;
-        
         std::copy(
             unique_results.begin(), unique_results.end(),
             std::back_inserter(results));
